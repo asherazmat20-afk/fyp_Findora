540f4337315d317b127fb86d7e0f7f56e68c6297
unknown <asherazmat20@gmail.com>
2026-05-20 09:28:01 +0500

    change AI to none

--- findora-backend/middleware/authMiddleware.js
+++ findora-backend/middleware/authMiddleware.js
@@ -4,6 +4,7 @@ module.exports = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
+    
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ error: "Unauthorized: token missing" });
     }
